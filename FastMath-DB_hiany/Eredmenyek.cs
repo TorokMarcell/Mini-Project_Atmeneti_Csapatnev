5693ff460415ca6022de0f517d714b9b46f2c0a0
katonaBalintSandor <114135507+katonaBalintSandor@users.noreply.github.com>
2022-10-12 20:07:11 +0200

    Program-99%

---
+++ FastMath-DB_hiany/Eredmenyek.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastMath
+{
+    public partial class Eredmenyek : Form
+    {
+
+        ScoreEntities sce = new ScoreEntities();
+
+
+        public Eredmenyek()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Eredmenyek_Load(object sender, EventArgs e)
+        {
+            //var query = from Table in sce.Table
+                        //select new { Table.Id, Table.Name, Table.Score };
+            //dgvScore.DataSource = query;
+        }
+    }
+}
---
+++ FastMath-DB_hiany/JatekH.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FastMath
+{
+    public partial class Form3 : Form
+    {
+
+        Random rnd = new Random();
+        string[] Maths = { "Add", "Subtract", "Multiply" };
+        int total;
+        public static int score;
+        static int i = 1;
+
+        public Form3()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void CheckAnswer(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtAnswer.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Csak számokat használj!");
+                txtAnswer.Text = txtAnswer.Text.Remove(txtAnswer.Text.Length - 1);
+            }
+        }
+
+        private void CheckButtonClickEvent(object sender, EventArgs e)
+        {
+            if (i == 10)
+            {
+                UserName u = new UserName();
+                u.ShowDialog();
+            }
+
+            int userEntered = Convert.ToInt32(txtAnswer.Text);
+
+            if (userEntered == total)
+            {
+                lblAnswer.Text = "Helyes!";
+                lblAnswer.ForeColor = Color.Green;
+                score += 1;
+                lblScore.Text = "Score: " + score;
+                i++;
+                SetUpGame();
+            }
+            else
+            {
+                lblAnswer.Text = "Helytelen!";
+                lblAnswer.ForeColor = Color.Red;
+                i++;
+                SetUpGame();
+            }
+
+
+        }
+
+        private void SetUpGame()
+        {
+            int numA = rnd.Next(10, 20);
+            int numB = rnd.Next(0, 9);
+            int csere;
+
+            txtAnswer.Text = null;
+
+            switch (Maths[rnd.Next(0, Maths.Length)])
+            {
+                case "Add":
+                    total = numA + numB;
+                    lblSymbol.Text = "+";
+                    lblSymbol.ForeColor = Color.DarkGreen;
+                    break;
+
+                case "Subtract":
+                    if (numB > numA)
+                    {
+                        csere = numA;
+                        numA = numB;
+                        numB = csere;
+                    }
+                    total = numA - numB;
+                    lblSymbol.Text = "-";
+                    lblSymbol.ForeColor = Color.Maroon;
+                    break;
+
+                case "Multiply":
+                    total = numA * numB;
+                    lblSymbol.Text = "x";
+                    lblSymbol.ForeColor = Color.Purple;
+                    break;
+            }
+
+            lblNumA2.Text = numA.ToString();
+            lblNumB2.Text = numB.ToString();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
